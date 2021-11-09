ed739b99ba3decdfb4adc2d63bf81ab022521c37
stasmruz <72268949+stasmruz@users.noreply.github.com>
2021-11-09 17:09:57 -0500

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Smoking_Data(
+index INTEGER,
+year INTEGER,
+Location VARCHAR(45),
+CigaretteSmokingPrevalence DOUBLE,
+TobaccoSmokingPrevalence DOUBLE,
+TobaccoUsePrevalence DOUBLE,
+MostSoldBrandCigaretteCurrency VARCHAR(5),
+MostSoldBrandCigarettePrice DOUBLE,
+rates DOUBLE,
+MostSoldUSD DOUBLE,
+lat DOUBLE,
+lng DOUBLE,
+PRIMARY KEY(index))

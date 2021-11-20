4187eeee024fef7ea5f2c816aabff8dbcce20971
stasmruz <72268949+stasmruz@users.noreply.github.com>
2021-11-20 15:12:42 -0500

    final

--- app.py
+++ app.py
@@ -57,10 +57,21 @@ def home():
 
 @app.route("/api/v1.0/machinelearning")
 def machine_learning():
+    country_str = "Location_Albania,Location_Algeria,Location_Andorra,Location_Argentina,Location_Armenia,Location_Australia,Location_Austria,Location_Azerbaijan,Location_Bangladesh,Location_Barbados,Location_Belarus,Location_Belgium,Location_Benin,Location_Bosnia and Herzegovina,Location_Brazil,Location_Burkina Faso,Location_Cambodia,Location_Canada,Location_Chad,Location_Chile,Location_China,Location_Colombia,Location_Comoros,Location_Congo,Location_Cook Islands,Location_Costa Rica,Location_Croatia,Location_Cuba,Location_Cyprus,Location_Czechia,Location_Côte d’Ivoire,Location_Democratic People's Republic of Korea,Location_Denmark,Location_Dominican Republic,Location_Egypt,Location_El Salvador,Location_Estonia,Location_Fiji,Location_Finland,Location_France,Location_Gambia,Location_Georgia,Location_Germany,Location_Ghana,Location_Greece,Location_Guyana,Location_Iceland,Location_India,Location_Indonesia,Location_Iran (Islamic Republic of),Location_Iraq,Location_Ireland,Location_Israel,Location_Italy,Location_Jamaica,Location_Japan,Location_Kazakhstan,Location_Kiribati,Location_Kuwait,Location_Kyrgyzstan,Location_Lao People's Democratic Republic,Location_Latvia,Location_Lebanon,Location_Lithuania,Location_Luxembourg,Location_Madagascar,Location_Mali,Location_Malta,Location_Mauritius,Location_Mexico,Location_Mongolia,Location_Morocco,Location_Mozambique,Location_Namibia,Location_Nauru,Location_Netherlands,Location_New Zealand,Location_Niger,Location_Norway,Location_Oman,Location_Pakistan,Location_Palau,Location_Panama,Location_Paraguay,Location_Peru,Location_Philippines,Location_Poland,Location_Portugal,Location_Qatar,Location_Republic of Korea,Location_Republic of Moldova,Location_Romania,Location_Russian Federation,Location_Rwanda,Location_Samoa,Location_Sao Tome and Principe,Location_Saudi Arabia,Location_Senegal,Location_Serbia,Location_Seychelles,Location_Sierra Leone,Location_Singapore,Location_Slovakia,Location_Slovenia,Location_Spain,Location_Togo,Location_Tuvalu,Location_Zimbabwe,Location_Botswana,Location_Burundi,Location_Cameroon,Location_Eritrea,Location_Eswatini,Location_Ethiopia,Location_Kenya,Location_Lesotho,Location_Liberia,Location_Myanmar,Location_Nepal,Location_Nigeria,Location_Timor-Leste,Location_United States of America"
+    country_list = country_str.split(",")
     model = pickle.load(open("model.pkl", "rb"))
     test_df = pd.read_csv("complete_xtest.csv")
+    actual = pd.read_csv("TestFile.csv")
+    actual_list = actual["CigaretteSmokingPrevalence"].tolist()
     prediction = model.predict(test_df)
-    return jsonify(f"{prediction}")
+    prediction = [int(p) for p in prediction]
+    actual_vs_pred = list(zip(actual_list, prediction))
+    actual_vs_pred = [list(a) for a in actual_vs_pred]
+    predictionary = dict(zip(country_list, actual_vs_pred))
+    for k,v in predictionary.items():
+        print(k,v, type(v))
+    return jsonify(predictionary)
+
 
 
 @app.route("/api/v1.0/country")
